77a298a0a12898e342962be0ede0c2c5e32ad53a
Rattikan Baimiden <117657803+rattikan-baimiden@users.noreply.github.com>
2023-04-13 02:51:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from function import calculate
 
 order = str(input("Enter your order :"))
-amount = int(input("Enter amount of order :"))
+amount = input("Enter amount of order :")
+try:
+    try:
+        amount = int(amount)
+    except:
+        amount = float(amount)
+except:
+    amount = str(amount)
 glass = str(input("Do you have glass y/n :"))
 
 result = calculate(order,amount,glass)
 print(result)
-# result = display_month(input_number)
-# print(result)
